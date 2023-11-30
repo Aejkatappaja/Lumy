71579f420c8dcf48fc36660a0470317b87ecc5eb
Aejkatappaja <joverjlj@gmail.com>
2023-11-30 14:36:20 +0100

    test(homepage): ui structure and Swiper test

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,152 @@
-import { getVideosList } from '@/lib/getVideos';
+'use client';
 
-export default async function Home() {
-  const response = await getVideosList();
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+import { useFetch } from '@/hooks/customs/useFetch';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+  const lastPublished = true;
+  const response = useFetch();
+  const { apiData } = response;
   console.log(response, 'data');
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 px-6'>
-      {response?.data?.map((item) => {
-        return <h1 key={item.title}> {item.title}</h1>;
-      })}
+    <main className='flex min-h-screen flex-col items-center justify-between gap-14 pl-12 pt-20'>
+      <Swiper
+        slidesPerView={2.5}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className='flex h-96 w-full border'
+      >
+        {apiData?.data?.map((item) => {
+          return (
+            <SwiperSlide key={item.title} className='h-96 w-1/2 border'>
+              <h1 key={item.title}> {item.title}</h1>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>{' '}
+      <div className='flex w-full flex-col space-y-4'>
+        <h1 className='border text-3xl font-bold'>DERNIERS REPLAYS</h1>
+        <Swiper
+          slidesPerView={2.5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className='flex h-96 w-full border'
+        >
+          {apiData?.data?.map((item) => {
+            return (
+              <SwiperSlide key={item.title} className='h-96 w-1/2 border'>
+                <h1 key={item.title}> {item.title}</h1>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>{' '}
+      </div>{' '}
+      <div className='flex w-full flex-col space-y-4'>
+        <h1 className='border text-3xl font-bold'>RETOUR SUR LES MATCHS</h1>
+        <Swiper
+          slidesPerView={2.5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className='flex h-96 w-full border'
+        >
+          {apiData?.data?.map((item) => {
+            return (
+              <SwiperSlide
+                key={item.title}
+                className='h-96 w-1/2 border'
+                onClick={() => router.push(`/video/${item.id}`)}
+              >
+                <h1 key={item.title}> {item.title}</h1>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>{' '}
+        <div className='mt-5 h-[483px] w-[96%] rounded-2xl bg-[#001131] '>
+          <div className=' relative flex h-[214px] flex-col '>
+            <Image
+              src='/images/bandeau_ehf_champions_league.jpg'
+              fill
+              alt='bandeau_ehf_champions_league'
+              className='rounded-t-2xl '
+            />{' '}
+          </div>
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className=' h-[232px] w-[96%] flex-1 border'
+          >
+            {apiData?.data?.map((item) => {
+              return (
+                <SwiperSlide key={item.title} className='h-full w-1/2 border'>
+                  <div className='h-[70%] w-full border'>x</div>
+                  <h1 key={item.title}> {item.title}</h1>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>{' '}
+        </div>
+      </div>
+      <div className='flex w-full flex-col space-y-4'>
+        <h1 className='border text-3xl font-bold'>LES PLUS REGARDES</h1>
+        <Swiper
+          slidesPerView={2.5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className='flex h-96 w-full border'
+        >
+          {apiData?.data?.map((item) => {
+            return (
+              <SwiperSlide key={item.title} className='h-96 w-1/2 border'>
+                <h1 key={item.title}> {item.title}</h1>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>{' '}
+      </div>{' '}
+      <div className='flex w-full flex-col space-y-4'>
+        <h1 className='border text-3xl font-bold'>TOUTES NOS PLAYLISTS</h1>
+        <Swiper
+          slidesPerView={2.5}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className='flex h-96 w-full border'
+        >
+          {apiData?.data?.map((item) => {
+            return (
+              <SwiperSlide key={item.title} className='h-96 w-1/2 border'>
+                <h1 key={item.title}> {item.title}</h1>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>{' '}
+      </div>{' '}
     </main>
   );
 }
