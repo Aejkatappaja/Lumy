7f88f6019e76f30b7401741436e2317515c5912f
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 00:49:07 +0100

    add(Card): react youtube player

--- components/video/Card.tsx
+++ components/video/Card.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import YouTube from 'react-youtube';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -9,6 +9,7 @@ import { Pagination } from 'swiper/modules';
 import { ITotalVideos } from '@/lib/getVideos';
 import { useRouter } from 'next/navigation';
 import { IPlaylists } from '@/lib/getPlaylists';
+import React from 'react';
 
 interface CardProps {
   apiData: ITotalVideos | IPlaylists | undefined;
@@ -18,6 +19,7 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ ...props }) => {
+  const [isLoading, setIsLoading] = React.useState(false);
   const router = useRouter();
   let slidesPerView: number;
   let breadcrumbs: boolean;
@@ -29,12 +31,16 @@ export const Card: React.FC<CardProps> = ({ ...props }) => {
     case 'lastReplay':
       slidesPerView = 4.2;
       breadcrumbs = false;
+    case 'playlists':
+      slidesPerView = 4.2;
+      breadcrumbs = false;
 
     default:
       slidesPerView = 3;
       breadcrumbs = false;
       break;
   }
+
   return (
     <div className='flex w-full flex-col space-y-6'>
       <div className='flex justify-between pr-12 font-bold'>
@@ -50,6 +56,32 @@ export const Card: React.FC<CardProps> = ({ ...props }) => {
           </span>{' '}
         </div>
       )}
+      <div className='flex h-96 gap-4 overflow-scroll border'>
+        {props.apiData?.data?.slice(0, 5).map((item) => {
+          return (
+            <div>
+              <div className='flex h-[80%] w-[20rem] border'>
+                {item?.youtube_id && (
+                  <YouTube
+                    videoId={item.youtube_id.toString()}
+                    onReady={() => setIsLoading(false)}
+                    className={isLoading ? 'hidden' : ''}
+                    loading='lazy'
+                  />
+                )}
+              </div>
+              <h1 className='font-bold'>{item.title}</h1>
+              {props.variant === 'playlists' &&
+                'video' in item &&
+                Array.isArray(item.video) && (
+                  <h2 className='oldGrey text-sm tracking-wide'>
+                    {item.video.length} vidéos{' '}
+                  </h2>
+                )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
