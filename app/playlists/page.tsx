40b6bfe1e4ddc17bf0e81dc76890e304168da372
Aejkatappaja <joverjlj@gmail.com>
2023-11-30 15:55:18 +0100

    add(playlist/id): ui

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -1,3 +1,20 @@
-export default function Playlist() {
-  return <div>Playlist</div>;
+import { strings } from '@/utils/strings';
+import { notFound } from 'next/navigation';
+
+export interface Iplaylist {
+  id: string;
+}
+
+export default function Playlist({ id }: Iplaylist) {
+  if (!id) {
+    notFound();
+  }
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='playlist-section border'>
+        <h1>{strings.playlists.all}</h1>
+      </div>
+      <div className='playlist-section border'>x</div>
+    </div>
+  );
 }
