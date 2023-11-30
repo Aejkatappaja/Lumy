a3f2b936cb318d6cc2549064c033aa8d07cbc60d
Aejkatappaja <joverjlj@gmail.com>
2023-11-30 14:18:36 +0100

    add(icons): footer icons func to map

---
+++ components/footer/icons.tsx
@@ -0,0 +1,32 @@
+export const icons = [
+  {
+    title: 'Facebook',
+    icon: '/images/Facebook.svg',
+    href: 'https://www.facebook.com/BBHOfficiel',
+  },
+  {
+    title: 'X',
+    icon: '/images/X.svg',
+    href: 'https://twitter.com/i/flow/login?redirect_after_login=%2FBBH_Officiel',
+  },
+  {
+    title: 'Instagram',
+    icon: './images/Instagram.svg',
+    href: 'https://www.instagram.com/bbh_officiel/?hl=fr',
+  },
+  {
+    title: 'Linkedin',
+    icon: './images/Linkedin.svg',
+    href: 'https://www.linkedin.com/company/bbh-business/?originalSubdomain=fr',
+  },
+  {
+    title: 'TikTok',
+    icon: './images/TikTok.svg',
+    href: 'https://www.tiktok.com/@bbhofficiel',
+  },
+  {
+    title: 'Youtube',
+    icon: './images/Youtube.svg',
+    href: 'https://www.youtube.com/c/brestbretagnehandballtv',
+  },
+];
