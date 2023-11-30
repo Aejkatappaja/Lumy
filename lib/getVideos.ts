5e49d622bf9f48827032db4a5bd95c7b793d5e9c
Aejkatappaja <joverjlj@gmail.com>
2023-11-30 17:43:19 +0100

    refacto(getVideos): conditional rendering

--- lib/getVideos.ts
+++ lib/getVideos.ts
@@ -19,16 +19,21 @@ export type IVideos = {
 
 export type ITotalVideos = { data: IVideos[] };
 export async function getVideosList(
-  lastPublished?: boolean
+  id?: string
 ): Promise<ITotalVideos | undefined> {
   const url = process.env.NEXT_PUBLIC_BASE_URL;
   console.log(url, 'url----------');
 
   try {
-    const res = await fetch(`${url}/items/video`, {
-      next: { revalidate: 0 },
-      method: 'GET',
-    });
+    const res = await fetch(
+      !id
+        ? `${url}/items/video`
+        : `https://api.brest.life/items/video?filter[id][_eq]=${id}`,
+      {
+        next: { revalidate: 0 },
+        method: 'GET',
+      }
+    );
 
     const response: ITotalVideos = await res.json();
 
