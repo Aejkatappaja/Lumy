4aac94aff4024ba7e344da3cddf2d150686d3d5c
Aejkatappaja <joverjlj@gmail.com>
2023-11-30 11:50:21 +0100

    test(fetch Videos): init call API try

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image';
+import { getVideosList } from '@/lib/getVideos';
 
-export default function Home() {
+export default async function Home() {
+  const response = await getVideosList();
+  console.log(response, 'data');
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 px-6'></main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 px-6'>
+      {response?.data?.map((item) => {
+        return <h1 key={item.title}> {item.title}</h1>;
+      })}
+    </main>
   );
 }
