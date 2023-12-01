4f2d30f1c8ca7f4bf7551c66870fdedb7c46a095
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 01:22:43 +0100

    add(playlist): titles

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -1,26 +1,24 @@
+import { getPlayList } from '@/lib/getPlaylists';
 import { strings } from '@/utils/strings';
 import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-
-export interface Iplaylist {
-  id: string;
-}
 
 export const metadata: Metadata = {
   title: 'BBH | Playlists',
   description: 'BBH Videos Playlists',
 };
 
-export default function Playlist({ id }: Iplaylist) {
-  if (!id) {
-    notFound();
-  }
+export default async function Playlist() {
+  const data = await getPlayList();
+
   return (
-    <div className='flex items-center justify-center'>
-      <div className='playlist-section border'>
-        <h1>{strings.playlists.all}</h1>
+    <div className='mx-auto mt-8 flex w-[80%] items-center  border'>
+      <div className='playlist-section font-Druk h-[535px] space-y-4 overflow-y-scroll border '>
+        <h1 className='pb-4 text-2xl'>{strings.playlists.all}</h1>
+        {data?.data?.map((item) => {
+          return <h2 className='text-4xl'>{item.title.toUpperCase()}</h2>;
+        })}
       </div>
-      <div className='playlist-section border'>x</div>
+      <div className='playlist-section border'></div>
     </div>
   );
 }
