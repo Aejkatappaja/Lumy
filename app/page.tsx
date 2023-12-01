d7b89f7dd4d4603e60e8478b7b8e7fd2ab470f27
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 12:56:59 +0100

    add(main page): suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,23 @@ import GetLastVideos from '@/components/GetLastVideos';
 import GetMostViewedVideos from '@/components/GetMostViewedVideos';
 import GetPlaylists from '@/components/GetPlaylists';
 import HeroEHF from '@/components/HeroEHF';
+import { Spinner } from '@/components/Spinner';
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center  gap-14 pl-12 pt-20'>
-      <GetLastVideos />
+      <Suspense fallback={<Spinner />}>
+        <GetLastVideos />
+      </Suspense>
       <HeroEHF />
-      <GetMostViewedVideos />
-      <GetPlaylists />
+      <Suspense fallback={<Spinner />}>
+        <GetMostViewedVideos />
+      </Suspense>
+      <Suspense fallback={<Spinner />}>
+        <GetPlaylists />
+      </Suspense>
     </main>
   );
 }
