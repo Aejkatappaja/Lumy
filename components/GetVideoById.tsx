564ab76ac6a663be66c9fe670272ab30fbcbe29e
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 17:49:32 +0100

    refacto(GetVideoById): destructuring

--- components/GetVideoById.tsx
+++ components/GetVideoById.tsx
@@ -7,7 +7,7 @@ export default async function GetVideoById({ id }: { id: string }) {
   const data = await getVideosList({ id });
   console.log(data, 'data');
   const ytId = data?.data[0]?.youtube_id.toString();
-  const url = `https://www.youtube.com/watch?v=${ytId}`;
+  const src = `https://www.youtube.com/watch?v=${ytId}`;
   return (
     <div className='mt-8 flex h-full flex-col px-24'>
       <div className='h-[681px]'>
@@ -17,7 +17,7 @@ export default async function GetVideoById({ id }: { id: string }) {
             <iframe
               width='100%'
               height='100%'
-              src={`https://www.youtube.com/embed/${ytId}`}
+              src={src}
               title='YouTube video player'
               frameBorder='0'
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
