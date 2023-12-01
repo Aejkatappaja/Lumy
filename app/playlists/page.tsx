5ef900ce8ce9cffa47202335dec7209929666100
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 17:31:21 +0100

    refacto(Playlist): UI

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -11,11 +11,17 @@ export default async function Playlist() {
   const data = await getPlayList();
 
   return (
-    <div className='mx-auto mt-8 flex w-[80%] items-center  border'>
-      <div className='playlist-section font-Druk h-[535px] space-y-4 overflow-y-scroll border '>
-        <h1 className='pb-4 text-2xl'>{strings.playlists.all}</h1>
+    <div className='mx-auto mt-8 flex w-[80%] items-center  border shadow-2xl shadow-black'>
+      <div className=' no-scrollbar h-[535px] space-y-8 overflow-y-scroll font-Druk'>
+        <h1 className='pb-4 text-2xl'>
+          {strings.playlists.page.toUpperCase()}
+        </h1>
         {data?.data?.map((item) => {
-          return <h2 className='text-4xl'>{item.title.toUpperCase()}</h2>;
+          return (
+            <h2 className=' oldGrey z-20 cursor-pointer text-4xl tracking-wide duration-700 hover:scale-75 hover:text-white'>
+              {item.title.toUpperCase()}
+            </h2>
+          );
         })}
       </div>
       <div className='playlist-section border'></div>
