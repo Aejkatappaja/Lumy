2148dcb31256ef67c73c7cc538d2be9b446ec8e6
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 12:56:41 +0100

    refacto(cards): UI & conditions

--- components/video/Card.tsx
+++ components/video/Card.tsx
@@ -7,6 +7,7 @@ import { ITotalVideos } from '@/lib/getVideos';
 import { IPlaylists } from '@/lib/getPlaylists';
 import React from 'react';
 import Image from 'next/image';
+import { Waves } from '@/utils/waves';
 
 interface CardProps {
   apiData: ITotalVideos | IPlaylists | undefined;
@@ -16,33 +17,42 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ ...props }) => {
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  let slidesPerView: number;
+  let shadowWaves: boolean;
   let breadcrumbs: boolean;
+  let pinkStuff: boolean;
   switch (props.variant) {
     case 'match':
-      slidesPerView = 2.45;
       breadcrumbs = true;
+      shadowWaves = false;
+      pinkStuff = false;
       break;
-    case 'lastReplay':
-      slidesPerView = 4.2;
+    case 'lastReplays':
       breadcrumbs = false;
+      shadowWaves = true;
+      pinkStuff = false;
+      break;
     case 'playlists':
-      slidesPerView = 4.2;
       breadcrumbs = false;
-
+      shadowWaves = false;
+      pinkStuff = false;
+      break;
+    case 'mostViewed':
+      breadcrumbs = false;
+      shadowWaves = false;
+      pinkStuff = true;
+      break;
     default:
-      slidesPerView = 3;
       breadcrumbs = false;
+      shadowWaves = false;
       break;
   }
 
   return (
-    <div className='flex w-full flex-col space-y-6'>
+    <div className='z-10 flex w-full flex-col space-y-6'>
+      {shadowWaves && <Waves />}
       <div className='flex justify-between pr-12 font-bold'>
         <h1 className='font-Druk text-4xl tracking-wide'>
-          {props.text.toUpperCase()}{' '}
+          {props.text.toUpperCase()}
         </h1>
         <button className='rounded-xl bg-[#404040] px-4 py-2'>Voir tout</button>
       </div>
@@ -50,19 +60,34 @@ export const Card: React.FC<CardProps> = ({ ...props }) => {
         <div className='flex'>
           <span className='rounded-3xl border bg-[#404040] px-4 py-2'>
             2013-15-12
-          </span>{' '}
+          </span>
         </div>
       )}
-      <div className='flex h-96 gap-10 overflow-x-scroll'>
+      <div className='flex h-96 gap-8 overflow-x-scroll'>
         {props.apiData?.data?.slice(0, 5).map((item) => {
+          const id = item.youtube_id.toString();
+          const url = `https://www.youtube.com/watch?v=${id}`;
           return (
-            <div className='flex flex-col justify-between '>
-              <div className=' flex h-[80%] w-[34rem] '>
+            <div className='relative flex flex-col justify-between' key={id}>
+              {' '}
+              <div
+                className={
+                  pinkStuff
+                    ? 'flex h-[80%] w-[24rem] flex-col justify-between '
+                    : 'flex h-[80%] w-[34rem] '
+                }
+              >
+                {' '}
+                {pinkStuff && (
+                  <div className='text z-10 flex h-24 w-full items-start justify-start  bg-gradient-to-b from-black from-15% via-[#9e2170] via-70% to-black to-25% pl-2 font-Druk text-5xl tracking-wider text-[#ff11aa]'>
+                    {item.view_count} vu
+                  </div>
+                )}
                 {props.variant !== 'playlists' ? (
                   item?.youtube_id && (
-                    <div className='no-scrollbar relative flex h-full w-full rounded-2xl border-2 border-white/20 bg-white/20 opacity-30 duration-1000 hover:opacity-100 hover:shadow-md hover:shadow-white/20'>
+                    <div className='no-scrollbar relative flex h-full w-full rounded-2xl border-2 border-white/20 bg-white/20 opacity-50 duration-1000  hover:opacity-100 hover:shadow-md hover:shadow-white/20'>
                       <ReactPlayer
-                        url={`https://www.youtube.com/watch?v=${item.youtube_id.toString()}`}
+                        url={url}
                         style={{ position: 'absolute', top: 20, left: 0 }}
                         width='100%'
                         height='85%'
@@ -80,7 +105,7 @@ export const Card: React.FC<CardProps> = ({ ...props }) => {
                     </div>
                   )
                 ) : (
-                  <div className='no-scrollbar relative flex h-full w-full items-center justify-center rounded-2xl border-2 border-white/20 bg-white/20 opacity-30 duration-1000 hover:opacity-100 hover:shadow-md hover:shadow-white/20'>
+                  <div className='no-scrollbar relative flex h-full w-full items-center justify-center rounded-2xl border-2 border-white/20 bg-white/20 opacity-50 duration-1000 hover:opacity-100 hover:shadow-md hover:shadow-white/20'>
                     <Image
                       src='/images/playlist-alt.jpeg'
                       fill
@@ -89,19 +114,21 @@ export const Card: React.FC<CardProps> = ({ ...props }) => {
                     />
                   </div>
                 )}
-              </div>
-              <h1 className='font-bold'>{item.title}</h1>
+              </div>{' '}
+              <h1 className='pl-2 font-bold'>{item.title}</h1>
               {props.variant === 'playlists' &&
-                'video' in item &&
-                Array.isArray(item.video) && (
-                  <h2 className='oldGrey text-sm tracking-wide'>
-                    {item.video.length} vidéos{' '}
-                  </h2>
-                )}
+              'video' in item &&
+              Array.isArray(item.video) ? (
+                <h2 className='oldGrey pl-2 text-sm tracking-wide'>
+                  {item.video.length} vidéos{' '}
+                </h2>
+              ) : (
+                <h2></h2>
+              )}
             </div>
           );
-        })}
-      </div>
+        })}{' '}
+      </div>{' '}
     </div>
   );
 };
