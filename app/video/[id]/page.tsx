71adf7783d5717de47b54ce3d233e5d938893d32
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 01:28:13 +0100

    delete(fetch): client side

--- app/video/[id]/page.tsx
+++ app/video/[id]/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { Text } from '@/components/video/Text';
-import { useCoverFetch } from '@/hooks/customs/useCoverFetch';
-import { useFetchVideo } from '@/hooks/customs/useFetchVideo';
+
 import { Metadata } from 'next';
 import { useParams } from 'next/navigation';
 
@@ -13,25 +12,17 @@ export const metadata: Metadata = {
 
 export default function Videos() {
   let cover;
-  const params = useParams();
-  const { id } = params;
-  const video = useFetchVideo(id.toString());
-  const { apiData, isLoading } = video;
-  if (video) {
-    cover = useCoverFetch(apiData?.data[0]?.cover as string);
-    console.log(cover?.apiData);
-  }
 
-  return !isLoading ? (
+  return (
     <div className='mt-8 flex h-full flex-col px-24'>
       <div className='h-[681px] bg-white '></div>
       <div className='flex h-[457px] items-center justify-center'>
         <section className='flex w-[768px] flex-col gap-[34px] '>
-          <h1 className='truncate text-5xl font-bold'>
+          {/* <h1 className='truncate text-5xl font-bold'>
             {apiData?.data[0]?.title}
-          </h1>{' '}
+          </h1>{' '} */}
           <Text text='Il y a 3 jours' />
-          <p>{apiData?.data[0]?.description}</p>
+          {/* <p>{apiData?.data[0]?.description}</p> */}
           <div className='flex space-x-6'>
             <div className='flex flex-col'>
               <h1 className='text-2xl font-bold'>1.4k </h1>
@@ -46,7 +37,5 @@ export default function Videos() {
         </section>
       </div>
     </div>
-  ) : (
-    <div>LOADING !</div>
   );
 }
