7efb689fa25de7389dc2f79c07412c58cd6aa52a
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 13:33:11 +0100

    add(main page): ThreeLastVideos component & BlurBackground

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import { BlurBackground } from '@/components/BlurBackground';
 import GetLastVideos from '@/components/GetLastVideos';
 import GetMostViewedVideos from '@/components/GetMostViewedVideos';
 import GetPlaylists from '@/components/GetPlaylists';
 import HeroEHF from '@/components/HeroEHF';
 import { Spinner } from '@/components/Spinner';
+import GetThreeLastVideos from '@/components/ThreeLastVideos';
 import Image from 'next/image';
 import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center  gap-14 pl-12 pt-20'>
+      <BlurBackground />
+      <Suspense fallback={<Spinner />}>
+        <GetThreeLastVideos />
+      </Suspense>{' '}
       <Suspense fallback={<Spinner />}>
         <GetLastVideos />
       </Suspense>
