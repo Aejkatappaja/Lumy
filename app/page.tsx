6aaf1607983abf109935317f48abfda333a11176
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 22:16:05 +0100

    refacto(main page): ui add margin top because of fixed header

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,11 @@ import GetLastVideos from '@/components/GetLastVideos';
 import GetMostViewedVideos from '@/components/GetMostViewedVideos';
 import GetPlaylists from '@/components/GetPlaylists';
 import HeroEHF from '@/components/HeroEHF';
-import { Spinner } from '@/components/Spinner';
 import GetThreeLastVideos from '@/components/ThreeLastVideos';
-import Image from 'next/image';
-import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center  gap-14 pl-12 pt-20'>
+    <main className='mt-10 flex min-h-screen flex-col  items-center gap-14 pl-12 pt-20'>
       <BlurBackground />
 
       <GetThreeLastVideos />
