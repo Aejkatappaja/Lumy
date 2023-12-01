ec4f29ccc31391bcb27ce251940321c63ec4f097
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 13:33:37 +0100

    refacto(EHF): UI

--- components/HeroEHF.tsx
+++ components/HeroEHF.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function HeroEHF() {
   return (
     <div className='flex w-full flex-col space-y-4'>
-      <div className='mt-5 h-[483px] w-[96%] rounded-2xl bg-[#001131] '>
+      <div className=' h-[483px] w-[96%] rounded-2xl bg-[#001131] '>
         <div className='relative flex h-[214px] flex-col'>
           <Image
             src='/images/bandeau_ehf_champions_league.jpg'
