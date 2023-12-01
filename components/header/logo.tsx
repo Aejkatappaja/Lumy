1cdf06671709af6e73a921f144a289c95470f501
Aejkatappaja <joverjlj@gmail.com>
2023-12-01 12:55:54 +0100

    refacto(logo): animate pulse

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image';
 
 export const Logo: React.FC = () => {
   return (
-    <Image src='/images/logo_bbh.svg' height={62} width={47} alt='bbh-logo' />
+    <Image
+      src='/images/logo_bbh.svg'
+      height={62}
+      width={47}
+      alt='bbh-logo'
+      className='animate-pulse'
+    />
   );
 };
