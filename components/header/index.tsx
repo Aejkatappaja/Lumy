c5e8d5e875d2e8c924976e7446e29cc5547ae0b6
Aejkatappaja <joverjlj@gmail.com>
2023-12-03 19:41:15 +0100

    responsive(header): apply changes to handle responsivle design

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -12,18 +12,27 @@ export const Header: React.FC = () => {
 
   return (
     <div
-      className='fixed left-0 right-0 top-0 z-50 transition-opacity duration-300 ease-in-out'
+      className='fixed left-0 right-0 top-0 z-50 w-screen transition-opacity duration-300 ease-in-out'
       style={{ backgroundColor: `rgba(0, 0, 0, ${opacity})` }}
     >
       <div className='flex h-[86px] items-center justify-between px-14'>
-        <nav className='space-x-6'>
+        <nav className='hidden space-x-6 md:flex'>
           {Links.map((link) => {
             const { href, title } = link;
             return <TextLink key={title} href={href} title={title} />;
           })}
         </nav>
-        <Logo />
-        <Button />
+        <div className='hidden md:block'>
+          <Logo />
+        </div>
+        <div className='flex w-full items-center justify-between md:hidden'>
+          <TextLink href={Links[0].href} title={Links[0].title} />
+          <Logo />
+          <TextLink href={Links[1].href} title={Links[1].title} />
+        </div>
+        <div className='hidden md:block'>
+          <Button />
+        </div>
       </div>
     </div>
   );
