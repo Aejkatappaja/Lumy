5fa6cfbfabe1dc934edc32bac48dc5d549bbab18
Aejkatappaja <joverjlj@gmail.com>
2023-12-03 19:41:43 +0100

    responsive(HeroEHF): apply changes to handle responsivle design

--- components/HeroEHF.tsx
+++ components/HeroEHF.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image';
 
 export default function HeroEHF() {
   return (
-    <div className='flex w-full flex-col space-y-4'>
-      <div className=' h-[483px] w-[96%] rounded-2xl bg-[#001131] '>
-        <div className='relative flex h-[214px] flex-col'>
+    <div className='flex w-full flex-col space-y-4 pl-2 md:pl-0'>
+      <div className=' w-[96%] rounded-2xl bg-[#001131] md:h-[483px] '>
+        <div className='relative flex h-[100px] flex-col md:h-[214px]'>
           <Image
             src='/images/bandeau_ehf_champions_league.jpg'
             fill
             alt='bandeau_ehf_champions_league'
-            className='rounded-t-2xl'
+            className='rounded-t-2xl md:object-cover'
           />{' '}
         </div>
-        <div className=' flex  overflow-scroll px-4 py-4'>
+        <div className=' md-gap-0 flex gap-4 overflow-scroll px-4 py-4'>
           {HeroData.map((item, index) => (
-            <div key={index} className='mx-auto flex flex-col gap-2 '>
+            <div key={index} className='mx-auto flex flex-col md:gap-2 '>
               <div className='relative h-48 w-80'>
                 <Image
                   src='/images/ehf-players.jpeg'
